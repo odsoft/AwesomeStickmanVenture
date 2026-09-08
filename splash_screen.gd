56e389e92501be6af8f6b43e89a6e20d5cb7c194
OdSoft <igortyma2009@gmail.com>
2026-09-08 16:38:27 +0200

    Removed dev version text from console

--- splash_screen.gd
+++ splash_screen.gd
@@ -3,7 +3,6 @@ extends Control
 func _ready() -> void:
 	print("Awesome Stickman Venture")
 	print(tr("log_made_by"))
-	print(tr("log_dev_ver"))
 	await get_tree().create_timer(0.5).timeout
 	$CanvasLayer/PresentsLabel.visible = true
 	await get_tree().create_timer(1.5).timeout
